aa8b226a709439949e2f454f16df29a9d0028719
Adrian Clay <adrian@madetech.com>
2018-04-20 14:13:42 +0100

    Add more useful cool page tests

--- spec/controllers/chirp_spec.rb
+++ spec/controllers/chirp_spec.rb
@@ -1,10 +1,62 @@
 require 'rails_helper'
 
 describe ChirpController do
+  render_views
+
   describe 'GET index' do
     it 'should set tweetz' do
-      get :index
-      expect(assigns(:sebs_tweets)).to all(be_a(Twitter::Tweet))
+      sleep 1 # don't hit rate limit 
+      begin
+        get :index
+        expect(assigns(:sebs_tweets)).to all(be_a(Twitter::Tweet))
+      rescue Twitter::Error::TooManyRequests
+        sleep 1 # don't hit rate limit
+        retry
+      end
+    end
+
+    it 'should set the scene' do
+      sleep 1 # don't hit rate limit
+      begin
+        get :index
+        expect(response.body).to include('Great Seb Tweets!')
+      rescue Twitter::Error::TooManyRequests
+        sleep 1 # don't hit rate limit
+        retry
+      end
+    end
+
+    it 'should show nice names' do
+      sleep 1 # don't hit rate limit
+      begin
+        get :index
+        expect(response.body).to include('<strong class="name">')
+      rescue Twitter::Error::TooManyRequests
+        sleep 1 # don't hit rate limit
+        retry
+      end
+    end
+
+    it 'should be a 200' do
+      sleep 1 # don't hit rate limit
+      begin
+        get :index
+        expect(response.status).to eq(200)
+      rescue Twitter::Error::TooManyRequests
+        sleep 1 # don't hit rate limit
+        retry
+      end
+    end
+
+    it 'should be a html page' do
+      sleep 1 # don't hit rate limit
+      begin
+        get :index
+        expect(response.headers['Content-Type']).to eq('text/html; charset=utf-8')
+      rescue Twitter::Error::TooManyRequests
+        sleep 1 # don't hit rate limit
+        retry
+      end
     end
   end
 
